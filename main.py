e2a3904976eb77cc6b9bbef1216415b5e4730585
pixelzery <pixelzery@gmail.com>
2021-01-25 10:58:06 +0000

    improve correction logic + implement unit testing Now handles cases such as URLs

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import discord
 import logging
 import datetime
+import re
+
+# regex from https://stackoverflow.com/a/3809435/5013267
+RE_URL = r"(?:http(s)?:\/\/.)?(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,21 +15,34 @@ else:
     with open("auth.txt", "r") as f:
         AUTH_TOKEN = f.read().strip()
 
-def needs_correcting(msg):
-    for word in msg.split(" "):
-        word = word.strip()
-        if len(word) < 2:
+def correct(msg):
+    """
+    WARNING: does not sanitise at this stage for comparison purposes
+    """
+    ret = ""
+    for word in re.split(r"(" + RE_URL + r"|.+?\b)", msg):
+        if word is None:
             continue
 
-        if any((c.isupper() for c in word[1:])):
-            if not all((c.isupper() for c in word)):
-                return True
-    return False
+        if len(word.strip()) > 2:
+            if all((
+                not word.isupper(),
+                not re.fullmatch(RE_URL, word),
+                not (word[-1] in ['s', 'z'] and word[:-1].isupper()),
+            )):
+                word = word[0] + word[1:].lower()
+        ret += word
+    return ret
 
 def sanitise(msg):
     # surround @ characters with ZWSP for safety
     return msg.replace("@", "\u200b@\u200b")
 
+def quote_text(msg):
+    txt = ""
+    for line in msg.splitlines(keepends=True):
+        txt += "> " + sanitise(line)
+    return txt
 
 class BotClient(discord.Client):
     async def login(self, *args, **kwargs):
@@ -33,6 +50,9 @@ class BotClient(discord.Client):
 
     async def on_ready(self):
         logging.info("Logged in as {}".format(self.user.name))
+
+        await self.change_presence(activity=discord.Game("git.io/JtZ5T"))
+        logging.info("Changed presence")
 
     async def on_message(self, message):
         if message.author == client.user:
@@ -45,17 +65,14 @@ class BotClient(discord.Client):
 
         logging.info("{}: Message: {}".format(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M"), message.content))
 
-        if needs_correcting(message.content):
+        corrected_msg = correct(message.content)
+        if message.content != corrected_msg:
             txt = ""
             txt += message.author.mention + "\n"
-
-            for line in message.content.splitlines(keepends=True):
-                txt += "> " + sanitise(line)
+            txt += quote_text(message.content)
 
             txt += "\n\nCorrected:\n"
-
-            for line in message.content.splitlines(keepends=True):
-                txt += "> " + sanitise(line.lower())
+            txt += quote_text(corrected_msg)
 
             await message.channel.send(txt)
             logging.info("corrected")
---
+++ test.py
@@ -0,0 +1,37 @@
+import unittest
+from main import correct
+
+class TestCorrection(unittest.TestCase):
+    def assertNeedsCorrecting(self, msg):
+        self.assertNotEqual(msg, correct(msg))
+
+    def assertNotNeedsCorrecting(self, msg):
+        self.assertEqual(msg, correct(msg))
+
+    def assertCorrection(self, msg, correction):
+        self.assertEqual(correct(msg), correction)
+
+    def test_normal(self):
+        self.assertNotNeedsCorrecting("hello there ! ")
+        self.assertNotNeedsCorrecting("hello  there")
+        self.assertNotNeedsCorrecting("hello     there")
+
+        self.assertNotNeedsCorrecting("Hi There This Is A Test")
+        self.assertNotNeedsCorrecting("ALL CAPS IS\nALL OKAY")
+        self.assertNotNeedsCorrecting("I like doing OOP, man")  # ALL CAPS WORDS mixed with all lowercase words is ok
+
+        self.assertNotNeedsCorrecting("I like doing OOP\nman I like OOP")  # split on \n too
+        self.assertNotNeedsCorrecting("I like doing OOP,man I like OOP")   # and split on , too
+        self.assertNotNeedsCorrecting("I like doing OOP!man I like OOP")   # etc..
+
+        self.assertNotNeedsCorrecting("Send those URLs for the LOLz")  # slightly strange but I'll allow it
+
+
+    def test_correction(self):
+        self.assertCorrection("HeLlO thEre", "Hello there")
+        self.assertCorrection("HeLlO  thEre ! ", "Hello  there ! ")
+
+        self.assertCorrection("Don't correct URLs like git.io/JtZ5T", "Don't correct URLs like git.io/JtZ5T")
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
